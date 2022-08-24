91b94d0e911d3bfb89fbcefa0a027a0121055f7c
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-08-24 22:22:35 +0300

    Add files via upload

---
+++ dice/Die.java
@@ -0,0 +1,24 @@
+package die;
+import java.util.*;
+public class Die {
+    private static final int max_number = 6;
+
+    private static final int min_number = 1;
+
+    private static final int no_number = 6;
+
+    private int number;
+    private Random random;
+
+    public Die (){
+        random = new Random();
+        number = no_number;
+    }
+    public void roll(){
+        number = random.nextInt(max_number-min_number+1) + min_number;
+    }
+    public int getNumber(){
+        return number;
+    }
+}   
+
